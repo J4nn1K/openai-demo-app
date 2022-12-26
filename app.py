a8f7cffb5551cf1c1c3a9233c0bf065164be9a23
J4nn1K <jannikjorge@grothusen.de>
2022-12-25 20:46:43 -0800

    Add basic ai completion functionality

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
 from flask import Flask, render_template, request
 
+#######################################
+#######################################
+
+import openai
+import os
+
+try:
+  openai.api_key = os.environ['OPENAI_API_KEY']
+except:
+  print("OPENAI_API_KEY not found")
+
+
+#######################################
+#######################################
+
 app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
 def index():
     if request.method == "POST":
       prompt = request.form.get("prompt")
-      print(prompt)
+      
+      response = openai.Completion.create(
+        model="text-curie-001",
+        prompt=prompt, 
+        temperature=0, 
+        max_tokens=20
+        )
+
+      print(response)
+
+      return render_template('index.html', prompt=prompt, response=response["choices"][0]["text"])
 
     return render_template('index.html')
