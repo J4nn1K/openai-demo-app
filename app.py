01338617ed82cfd9ddae17e2034d564bdef9b1df
J4nn1K <jannikjorge@grothusen.de>
2022-12-25 23:12:15 -0800

    Finalize text completion and image generation

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 from flask import Flask, render_template, request
-
-#######################################
-#######################################
-
 import openai
 import os
 
@@ -11,26 +7,42 @@ try:
 except:
   print("OPENAI_API_KEY not found")
 
-
-#######################################
-#######################################
-
 app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
 def index():
     if request.method == "POST":
-      prompt = request.form.get("prompt")
+
+      print(request.form.get("completion_prompt"))
       
-      response = openai.Completion.create(
-        model="text-curie-001",
-        prompt=prompt, 
-        temperature=0, 
-        max_tokens=20
+      if request.form.get("completion_prompt"):
+        completion_prompt = request.form.get("completion_prompt")
+        completion_model = request.form.get("completion_model")
+
+        response = openai.Completion.create(
+          model=completion_model,
+          prompt=completion_prompt, 
+          temperature=0, 
+          max_tokens=3
+          )
+
+        print(response)
+
+        return render_template('index.html', completion_prompt=completion_prompt, completion_response=response["choices"][0]["text"])
+
+      if request.form.get("image_prompt"):
+        image_prompt = request.form.get("image_prompt")
+
+        print(image_prompt)
+
+        response = openai.Image.create(
+          prompt=image_prompt,
+          n=1,
+          size="512x512"
         )
 
-      print(response)
+        print(response)
 
-      return render_template('index.html', prompt=prompt, response=response["choices"][0]["text"])
-
+        return render_template('index.html', image_prompt=image_prompt, image_url=response['data'][0]['url'])
+    
     return render_template('index.html')
