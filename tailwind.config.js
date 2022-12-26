7033da7f955d292d937a4602e001c4f1992c2ed6
J4nn1K <jannikjorge@grothusen.de>
2022-12-26 02:04:41 -0800

    Add Roboto Mono font

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        mono: ['Roboto Mono', ...defaultTheme.fontFamily.mono]
       },
     },
   },
